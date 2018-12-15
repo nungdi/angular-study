d69ee543f9c2afb627e8ecaf95a7497aceb2d144
nungdi <seunj87@gmail.com>
2018-12-15 11:03:31 +0900

    todo angular 방식으로 구현

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import {HEROES} from './mock-heroes';
-import {Hero} from './hero';
-import {Observable, of, Subject} from 'rxjs';
-import {delay} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { Observable, of, Subject } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { TodoVo } from './domain/todo.vo';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,9 @@ export class HeroService {
     // const hero = HEROES.find(item => item.hero_id === hero_id ? true : false);
     // return of(hero);
     return this.http.get<Hero>(`${environment.HOST}/api/hero/${hero_id}`);
+  }
+
+  getTodoList(): Observable<TodoVo[]> {
+    return this.http.get<TodoVo[]>(`${environment.HOST}/api/todo`);
   }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { TodoVo } from '../domain/todo.vo';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  todoList: TodoVo[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getTodoList()
+      .subscribe(body => {
+        console.log(body);
+        this.todoList = body;
+      });
   }
-
 }
