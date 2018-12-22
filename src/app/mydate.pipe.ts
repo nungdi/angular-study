df929e67cdc5ea396198b27ea3bd35b326da9b7c
nungdi <seunj87@gmail.com>
2018-12-22 10:11:44 +0900

    angular 날짜 pipe 사용하기

--- src/app/mydate.pipe.ts
+++ src/app/mydate.pipe.ts
@@ -5,17 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MydatePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    // 첫번째 파라미터는 파이프 앞의 변수
-    console.log('first value:', value);
-    // 두번째 파라미터는 mydata: 다음에 오는 변수
-    console.log('second args:', args);
-    return value + 'aaa';
-    // if (args === '$') {
-    //   return (value as number) / 1100 + args;
-    // } else if (args === 'yen') {
-    //   return (value as number) / 120 + args;
-    // }
+  transform(value: any, ...args: any): any {
+    // 첫번째 파라메터는 파이프 앞의 변수
+    // 두번째 파라메터는 mydate: 다음에 오는 변수
+    console.log('first value: ', value);
+    console.log('second agrs: ', args );
+
+    // 환율 변환 예제
+    /*    let result = 0;
+        switch (args) {
+          case '$':
+            result = (value as number) / 1100
+            break;
+          case 'yen':
+            result = (value as number) / 120
+            break;
+        }
+        return result;*/
+
+    // 날짜 변환
+    return (value as string).substring(0, 16);
   }
 
 }
